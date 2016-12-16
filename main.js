1090a1387672fb545e89b0bc2f999147c555e1fa
Sascha Hoppe <kn0rki@users.noreply.github.com>
2016-12-16 22:23:30 +0100

    Added Autospawn

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    var numCreeps = Game.spawns["Spawn1"].room.find(FIND_MY_CREEPS);
+
+            Game.spawns["Spawn1"].createCreep([WORK, CARRY, MOVE], null, {role: "harvester"});
+        } else {
+    }
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
 
-        if(Game.spawn['Spawn1'].energy === Game.spawn['Spawn1'].energyCapacity) {
+       if(Game.spawns["Spawn1"].energy === Game.spawns["Spawn1"].energyCapacity) {
             roleUpgrader.run(creep);
         } else {
             if(creep.memory.role == 'harvester') {
@@ -16,7 +28,7 @@ module.exports.loop = function () {
                 roleUpgrader.run(creep);
             }
         }
-
+       
         
     }
 }
