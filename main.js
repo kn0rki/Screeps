e05fff666363a3d37a32fa1050a35bbca214dba6
Sascha Hoppe <kn0rki@users.noreply.github.com>
2016-12-17 03:51:48 +0100

    broken stuff.

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
+var roleHealer = require('role.healer');
+var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
     for(var name in Memory.creeps) {
@@ -9,11 +11,13 @@ module.exports.loop = function () {
         }
     }
 
+   
+
     var numCreeps = Game.spawns["Spawn1"].room.find(FIND_MY_CREEPS);
     //console.log("Creep Anzahl " + numCreeps.length);
 
      if(numCreeps.length < 5) {
-            //console.log("Creating new Harvester");
+            console.log("Creating new Harvester");
             Game.spawns["Spawn1"].createCreep([WORK, CARRY, MOVE], null, {role: "harvester"});
         } else {
            // console.log("test");
@@ -21,12 +25,24 @@ module.exports.loop = function () {
 
     for(var name in Game.creeps) {
         var creep = Game.creeps[name];
+        var carryTotal = _.sum(creep.carry);
+        var targets = creep.room.find(FIND_STRUCTURES, {
+            filter: object => object.hits < object.hitsMax
+        });
+        console.log(creep.name + " " + carryTotal + "/" +creep.carryCapacity);
 
        if(Game.spawns["Spawn1"].energy === Game.spawns["Spawn1"].energyCapacity) {
-            roleUpgrader.run(creep);
-        } else {
+         
+           //if(carryTotal > 0){
+                roleBuilder.run(creep);
+                creep.say("Builder");
+            //}
+            
+        
+       } else {
             if(creep.memory.role == 'harvester') {
                 roleHarvester.run(creep);
+                creep.say("Harvester");
             }
             if(creep.memory.role == 'upgrader') {
                 roleUpgrader.run(creep);
@@ -35,4 +51,12 @@ module.exports.loop = function () {
        
         
     }
+
+    
+    if(targets.length > 0) {
+    if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(targets[0]);    
+    }
+}
+
 }
--- role.harvester.js
+++ role.harvester.js
@@ -2,15 +2,35 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
+        
         if(creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+            var targets = creep.room.find(FIND_DROPPED_RESOURCES);
+            if(targets.length) {
+                creep.moveTo(targets[0]);
+                creep.pickup(targets[0]);
+               // console.log("foo0");
+            }else{
+                var sources = creep.room.find(FIND_SOURCES);
+                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0]);
+                }
+                //console.log(creep.name + " foo1");
             }
-        }
-        else if(Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
-            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['Spawn1']);
+        } else {
+            //console.log(creep.name + " foo2");
+            var structargets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION ||
+                                structure.structureType == STRUCTURE_SPAWN ||
+                                structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
+                    }
+            });
+            //console.log("foo " + structargets.length);
+            if(structargets.length > 0) {
+                if(creep.transfer(structargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(structargets[0]);
+                   // console.log("foo " + structargets.length);
+                }
             }
         }
     }
